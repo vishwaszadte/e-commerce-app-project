dee9e317fbae5aa60a63f30a792a78811b79ae29
Vishwas Zadte <vishwaszadte@gmail.com>
2022-06-16 14:09:51 +0530

    minor changes for heroku

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const transporter = mailjet.connect(
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // setting ejs as the default templating engine
 // by default this uses views directory
