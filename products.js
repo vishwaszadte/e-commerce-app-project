b3740d67442d0e2b09a4702689d972c208d71419
Vishwas Zadte <vishwaszadte@gmail.com>
2022-07-21 09:24:01 +0530

    some changes in the frontend

--- products.js
+++ products.js
@@ -1,101 +1,101 @@
 [{
     "name": "Videocon Tv",
-    "price": "theek theek",
-    "description": "sahi hai bada sa",
+    "price": "21,999",
+    "description": "A perfect TV designed for your living room",
     "_id": "1"
 }, {
-    "name": "iPhone",
-    "price": "Mehenga",
+    "name": "iPhone 11",
+    "price": "51,999",
     "description": "One of the best phones available",
     "_id": "2"
 }, {
     "name": "LG Washing Machine",
-    "price": "Thoda mehengi side pe",
-    "description": "chal jaata hai",
+    "price": "18,999",
+    "description": "Giving your clothes, the best wash possible",
     "_id": "3"
 }, {
     "name": "Hyundai Car",
-    "price": "Baapre bohot zyada",
-    "description": "EV le lo, internal combustion engine thode saalo baad museum mei milega",
+    "price": "2,19,999",
+    "description": "The best driving experience",
     "_id": "4"
 }, {
     "name": "Amul Icecream",
-    "price": "Sasta",
-    "description": "garmi hai, le lo",
+    "price": "25",
+    "description": "Your saviour in the hot summer",
     "_id": "5"
 }, {
     "name": "Videocon Tv",
-    "price": "theek theek",
-    "description": "sahi hai bada sa",
+    "price": "21,999",
+    "description": "A perfect TV designed for your living room",
     "_id": "6"
 }, {
-    "name": "iPhone",
-    "price": "Mehenga",
+    "name": "iPhone 11",
+    "price": "51,999",
     "description": "One of the best phones available",
     "_id": "7"
 }, {
     "name": "LG Washing Machine",
-    "price": "Thoda mehengi side pe",
-    "description": "chal jaata hai",
+    "price": "18,999",
+    "description": "Giving your clothes, the best wash possible",
     "_id": "8"
 }, {
     "name": "Hyundai Car",
-    "price": "Baapre bohot zyada",
-    "description": "EV le lo, internal combustion engine thode saalo baad museum mei milega",
+    "price": "2,19,999",
+    "description": "The best driving experience",
     "_id": "9"
 }, {
     "name": "Amul Icecream",
-    "price": "Sasta",
-    "description": "garmi hai, le lo",
+    "price": "25",
+    "description": "Your saviour in the hot summer",
     "_id": "10"
 }, {
     "name": "Videocon Tv",
-    "price": "theek theek",
-    "description": "sahi hai bada sa",
+    "price": "21,999",
+    "description": "A perfect TV designed for your living room",
     "_id": "11"
 }, {
-    "name": "iPhone",
-    "price": "Mehenga",
+    "name": "iPhone 11",
+    "price": "51,999",
     "description": "One of the best phones available",
     "_id": "12"
 }, {
     "name": "LG Washing Machine",
-    "price": "Thoda mehengi side pe",
-    "description": "chal jaata hai",
+    "price": "18,999",
+    "description": "Giving your clothes, the best wash possible",
     "_id": "13"
 }, {
     "name": "Hyundai Car",
-    "price": "Baapre bohot zyada",
-    "description": "EV le lo, internal combustion engine thode saalo baad museum mei milega",
+    "price": "2,19,999",
+    "description": "The best driving experience",
     "_id": "14"
 }, {
     "name": "Amul Icecream",
-    "price": "Sasta",
-    "description": "garmi hai, le lo",
+    "price": "25",
+    "description": "Your saviour in the hot summer",
     "_id": "15"
 }, {
     "name": "Videocon Tv",
-    "price": "theek theek",
-    "description": "sahi hai bada sa",
+    "price": "21,999",
+    "description": "A perfect TV designed for your living room",
     "_id": "16"
 }, {
-    "name": "iPhone",
-    "price": "Mehenga",
+    "name": "iPhone 11",
+    "price": "51,999",
     "description": "One of the best phones available",
     "_id": "17"
 }, {
     "name": "LG Washing Machine",
-    "price": "Thoda mehengi side pe",
-    "description": "chal jaata hai",
+    "price": "18,999",
+    "description": "Giving your clothes, the best wash possible",
     "_id": "18"
 }, {
     "name": "Hyundai Car",
-    "price": "Baapre bohot zyada",
-    "description": "EV le lo, internal combustion engine thode saalo baad museum mei milega",
+    "price": "2,19,999",
+    "description": "The best driving experience",
     "_id": "19"
 }, {
     "name": "Amul Icecream",
-    "price": "Sasta",
-    "description": "garmi hai, le lo",
+    "price": "25",
+    "description": "Your saviour in the hot summer",
     "_id": "20"
 }]
